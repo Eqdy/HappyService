3f2d88ac137029dcc007d04e0eae50e9fc091023
Eqdy <edisonremache@gmail.com>
2017-07-09 18:29:44 -0500

    Inicio

---
+++ HappyService/src/main/java/com/happyPaw/ws/rest/service/ServiceLogin.java
@@ -0,0 +1,26 @@
+package com.happyPaw.ws.rest.service;
+
+
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import com.happyPaw.ws.rest.vo.VOUsuario;
+
+@Path("/PruebaRs")
+public class ServiceLogin {
+	@POST
+	@Path("/validarUsuario")
+	@Consumes({MediaType.APPLICATION_JSON})
+	@Produces({MediaType.APPLICATION_JSON})
+	public String validarUsuario(){
+//		vo.setUserValido(false);
+//		if(vo.getUsuario().equals("ER") && vo.getPassword().equals("12345")){
+//			vo.setUserValido(true);
+//		}
+		return "listo";
+	}
+}
